d720f80ccf8ed193a3151baa2850c5064aedcdb7
D6200763-SUT <d6200763@g.sut.ac.th>
2025-05-20 07:19:38 +0700

    cm

--- Creatdata.py
+++ Creatdata.py
@@ -7,13 +7,13 @@ file_path = '/Users/np_sut/project_python_github/Thermal_modeling_2024/Thermal_m
 df = pd.read_csv(file_path)
 
 # 1. กรองข้อมูล
-high_sales = df[df['Sales'] > 1000]
+#high_sales = df[df['Sales'] > 1000]
 
 # 2. คำนวณยอดขายรวมตามภูมิภาค
-sales_by_region = df.groupby('Region')['Sales'].sum()
+#sales_by_region = df.groupby('Region')['Sales'].sum()
 
 # 3. บันทึกผลลัพธ์
-high_sales.to_csv('high_sales.csv', index=False)
-sales_by_region.to_csv('sales_by_region.csv')
+#high_sales.to_csv('high_sales.csv', index=False)
+#sales_by_region.to_csv('sales_by_region.csv')
 
-print("การประมวลผลข้อมูลเสร็จสิ้น")
+#print("การประมวลผลข้อมูลเสร็จสิ้น")
